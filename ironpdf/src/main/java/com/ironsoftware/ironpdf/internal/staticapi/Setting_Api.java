8ed9a66eae667e5be786cfc57052f12d2f518edd
DevOps <noreply@ironsoftware.com>
2026-02-10 03:26:14 +0000

    Copy from IronPolyglot 2026-02-10

--- ironpdf/src/main/java/com/ironsoftware/ironpdf/internal/staticapi/Setting_Api.java
+++ ironpdf/src/main/java/com/ironsoftware/ironpdf/internal/staticapi/Setting_Api.java
@@ -19,7 +19,7 @@ public final class Setting_Api {
 
     public static IronPdfEngineConnection connectionMode = IronPdfEngineConnection.configure().withSubprocess();
 
-    public static final String IRON_PDF_ENGINE_VERSION = "2025.12.2";
+    public static final String IRON_PDF_ENGINE_VERSION = "2026.1.3";
 
     public static int ironPdfEngineTimeout = 120;
 
